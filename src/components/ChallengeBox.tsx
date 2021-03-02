7eebdb9cea8c5a7b5c2c615751f7b5357dfb5614
João Victor <joaovictordjardim@gmail.com>
2021-03-02 16:34:48 -0300

    Funcao startNewChallenge configurada

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -3,22 +3,18 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox () {
-  const contextData = useContext(ChallengesContext);
-
-  console.log(contextData);
-
-  const hasActiveChallenge = true;
+  const { activeChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400xp</header>
+          <header>Ganhe {activeChallenge.amount}xp</header>
 
           <main>
-            <img src="icons/body.svg" alt=""/>
+            <img src={`icons/${activeChallenge.type}.svg`} alt=""/>
             <strong>Novo Desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos.</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
