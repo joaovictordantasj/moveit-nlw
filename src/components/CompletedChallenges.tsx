0a199a0fa7d60e95422b8dd9f1528416c64eb5da
João Victor <joaovictordjardim@gmail.com>
2021-03-03 15:36:22 -0300

    Funcao completeChallenge configurada

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompletedChallanges.module.css';
+import challenges from '../../challenges.json';
 
 export function CompletedChallenges () {
   const { challengesCompleted } = useContext(ChallengesContext);
@@ -8,7 +9,7 @@ export function CompletedChallenges () {
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios Completos</span>
-      <span>{challengesCompleted}</span>
+      <span>{challengesCompleted}/{challenges.length}</span>
     </div>
   );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile () {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.ProfileContainer}>
       <img src="https://github.com/joaovictordantasj.png" alt="imagem de perfil"/>
@@ -8,7 +12,7 @@ export function Profile () {
         <strong>João Victor</strong>
         <p>
           <img src="icons/level.svg" alt="Level Icon"/>  
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
